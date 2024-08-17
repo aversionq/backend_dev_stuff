4d50bf39e70c0c51857931896984e8ea19aaec6c
DESKTOP-0QP0GE6\arskz <arskzxc@gmail.com>
2024-08-17 18:03:31 +0400

    добавил паттерн синглтон

---
+++ dev_csharp/patterns/BackendDevStuff.Patterns/BackendDevStuff.Patterns.Singleton/SingletonClass.cs
@@ -0,0 +1,16 @@
+﻿namespace BackendDevStuff.Patterns.Singleton;
+
+public class SingletonClass
+{
+    private static readonly SingletonClass _instance = new SingletonClass();
+
+    private SingletonClass()
+    {
+        
+    }
+
+    public static SingletonClass GetInstance()
+    {
+        return _instance;
+    }
+}
